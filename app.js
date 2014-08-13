b4c67cb3b7a6310d28a409478c6f70ecf80569f7
Adrian Green <adriangreen@Adrians-MacBook-Air.local>
2014-08-13 11:56:43 -0400

    adding route to notes in main app.js

--- app.js
+++ app.js
@@ -35,6 +35,20 @@ app.put('/query', function(req,res){
         res.send({data:response});
     })
 })
+app.put('/queryNotes', function(req,res){
+    console.log(req.body);
+    res.setHeader("Content-Type", "text/html");
+    reports.queryNotes(req.body.name,req.body.month, function(err,response){
+        res.send({data:response});  
+    })
+})
+app.put('/export', function(req,res){
+    console.log(req.body);
+    res.setHeader("Content-Type", "text/html");
+    reports.dl(req.body.name,req.body.month, function(err,response){
+        res.send({data:"got it"})
+    })
+})
 app.get('/report', routes.reports);
 app.get('/partials/:name', routes.partials);
 app.get('*', routes.index);
